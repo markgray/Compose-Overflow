c722e7a7180dd517f6df46dfec577bbbb745b03c
markgray <mark.t.gray@gmail.com>
2025-01-28 07:19:57 -0500

    Added comments in Jetcaster

--- Jetcaster/core/data/src/main/java/com/example/jetcaster/core/data/database/dao/TransactionRunnerDao.kt
+++ Jetcaster/core/data/src/main/java/com/example/jetcaster/core/data/database/dao/TransactionRunnerDao.kt
@@ -19,15 +19,35 @@ package com.example.jetcaster.core.data.database.dao
 import androidx.room.Dao
 import androidx.room.Ignore
 import androidx.room.Transaction
+import com.example.jetcaster.core.data.repository.PodcastsRepository
 
 /**
- * [Room] DAO which provides the implementation for our [TransactionRunner].
+ * `Room` DAO which provides the implementation for our [TransactionRunner]. It is injected by
+ * `Hilt` to be used by [PodcastsRepository] to run multiple `Room` operations in an atomic
+ * `@Transaction`.
  */
 @Dao
 abstract class TransactionRunnerDao : TransactionRunner {
+    /**
+     * This method runs its `lambda` parmameter [tx] in an atomic `@Transaction`. It is not used
+     * directly, instead it is called by invoking an instance of [TransactionRunner] as if it were
+     * a function. The `@Transaction` annotation is the key to ensuring database integrity. It
+     * guarantees that either all the operations within the lambda succeed, or none of them do.
+     *
+     * @param tx `suspend` Lambda that should be executed inside an atomic `@Transaction`.
+     */
     @Transaction
-    protected open suspend fun runInTransaction(tx: suspend () -> Unit) = tx()
+    protected open suspend fun runInTransaction(tx: suspend () -> Unit): Unit = tx()
 
+    /**
+     * [invoke] is a very Kotlin-idiomatic way to make a `TransactionRunner` instance callable like
+     * a function. It makes the code more concise and readable. The `@Ignore` annotation is used to
+     * prevent Room from generating an implementation for the invoke function. This is because the
+     * implementation is provided in the abstract class. It just calls our protected [runInTransaction]
+     * method with its lambda parameter [tx].
+     *
+     * @param tx `suspend` Lambda that should be executed inside an atomic `@Transaction`.
+     */
     @Ignore
     override suspend fun invoke(tx: suspend () -> Unit) {
         runInTransaction(tx)
@@ -35,9 +55,14 @@ abstract class TransactionRunnerDao : TransactionRunner {
 }
 
 /**
- * Interface with operator function which will invoke the suspending lambda within a database
+ * Interface with operator function which will invoke the suspending lambda within a single database
  * transaction.
  */
 interface TransactionRunner {
+    /**
+     * Invoke the [tx] lambda within an atomic database transaction.
+     *
+     * @param tx `suspend` Lambda that should be executed inside an atomic `@Transaction`.
+     */
     suspend operator fun invoke(tx: suspend () -> Unit)
 }
