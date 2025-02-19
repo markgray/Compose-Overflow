6855946c37c50a67962b4bd7b0a5cc486c182be1
markgray <mark.t.gray@gmail.com>
2025-02-19 07:27:14 -0500

    Added comments in Jetcaster

--- Jetcaster/core/domain/src/main/java/com/example/jetcaster/core/player/EpisodePlayer.kt
+++ Jetcaster/core/domain/src/main/java/com/example/jetcaster/core/player/EpisodePlayer.kt
@@ -80,40 +80,84 @@ interface EpisodePlayer {
      */
     var playerSpeed: Duration
 
+    /**
+     * Adds a [PlayerEpisode] to the playback queue.
+     *
+     * This function takes a [PlayerEpisode] as input and appends it to the end of the current
+     * playback queue. The queue represents the sequence of episodes that will be played.
+     *
+     * @param episode The [PlayerEpisode] to be added to the queue.
+     *
+     * @see PlayerEpisode
+     * @see removeAllFromQueue
+     */
     fun addToQueue(episode: PlayerEpisode)
 
-    /*
-    * Flushes the queue
-    */
+    /**
+     * Removes all elements from the queue, effectively emptying it.
+     *
+     * After calling this function, the queue will have a size of 0 and will be considered empty.
+     * Any subsequent attempts to peek or dequeue from the queue will result in an exception or
+     * undefined behavior depending on the queue's specific implementation.
+     *
+     * This operation modifies the queue in-place.
+     *
+     * @throws IllegalStateException if the queue is not modifiable (e.g., if it's a fixed-size
+     * queue that doesn't support removal) or if any other error occurs that prevents clearing the
+     * queue. The specific exception thrown may depend on the queue's implementation.
+     */
     fun removeAllFromQueue()
 
     /**
-     * Plays the current episode
+     *  Plays the current episode.
      */
     fun play()
 
     /**
-     * Plays the specified episode
+     * Initiates playback of the provided [PlayerEpisode].
+     *
+     * This function is responsible for starting the playback of a specific episode
+     * within the player. It takes a [PlayerEpisode] object as input, which
+     * encapsulates all the necessary information about the episode to be played,
+     * such as its content URI, title, and other relevant metadata.
+     *
+     * The function handles tasks such as:
+     * - Preparing the media player with the episode's content URI.
+     * - Starting the playback process.
+     * - Updating the UI to reflect the current playback state.
+     * - Handling any necessary pre-playback checks or setup.
+     * - Managing playback errors and interruptions.
+     *
+     * @param playerEpisode The [PlayerEpisode] object containing the details of the episode to be played.
+     * @see PlayerEpisode
      */
     fun play(playerEpisode: PlayerEpisode)
 
     /**
-     * Plays the specified list of episodes
+     * Initiates playback of a list of player episodes.
+     *
+     * This function takes a list of [PlayerEpisode] objects and starts playing them sequentially.
+     * It's responsible for handling the playback order and potentially any associated logic
+     * like pre-loading or handling end-of-episode events.
+     *
+     * @param playerEpisodes A [List] of [PlayerEpisode] objects representing the content to
+     * be played. Must not be empty. If empty an exception will be thrown.
+     * @throws IllegalArgumentException if the provided list [playerEpisodes] is empty.
      */
     fun play(playerEpisodes: List<PlayerEpisode>)
 
     /**
-     * Pauses the currently played episode
+     * Pauses the currently playing episode.
      */
     fun pause()
 
     /**
-     * Stops the currently played episode
+     * Stops the currently playing episode
      */
     fun stop()
 
     /**
-     * Plays another episode in the queue (if available)
+     * Plays the next episode in the queue (if available)
      */
     fun next()
 
@@ -124,32 +168,51 @@ interface EpisodePlayer {
     fun previous()
 
     /**
-     * Advances a currently played episode by a given time interval specified in [duration].
+     * Advances a currently played episode by the time interval specified by [Duration] parameter
+     * [duration].
      */
     fun advanceBy(duration: Duration)
 
     /**
-     * Rewinds a currently played episode by a given time interval specified in [duration].
+     * Rewinds a currently played episode by the time interval specified by [Duration] parameter
+     * [duration].
      */
     fun rewindBy(duration: Duration)
 
     /**
-     * Signal that user started seeking.
+     * Called when the user starts seeking within the media.
+     *
+     * This function is invoked when the user initiates a seek operation, such as dragging a slider
+     * or using fast-forward/rewind controls. It signifies the beginning of a seek action, before
+     * the actual seek position is finalized.
+     *
+     * This is useful for:
+     *  - Displaying a loading indicator or updating the UI to reflect the ongoing seek.
+     *  - Pausing or muting audio/video temporarily during the seek operation.
+     *  - Canceling any ongoing timed operations that might conflict with the seek.
      */
     fun onSeekingStarted()
 
     /**
-     * Seeks to a given time interval specified in [duration].
+     * Seeks to the time interval specified by [Duration] parameter [duration].
      */
     fun onSeekingFinished(duration: Duration)
 
     /**
-     * Increases the speed of Player playback by a given time specified in [duration].
+     * Increases the speed of Player playback by the time specified by [Duration] parameter [speed].
+     *
+     * @param speed The duration representing the speed increase. Defaults to 500 milliseconds. A
+     * shorter duration represents a faster increase in speed. Must be a non-negative duration.
+     * @throws IllegalArgumentException if the provided speed duration is negative.
      */
     fun increaseSpeed(speed: Duration = Duration.ofMillis(500))
 
     /**
-     * Decreases the speed of Player playback by a given time specified in [duration].
+     * Decreases the speed of Player playback by the time specified by [Duration] parameter [speed].
+     *
+     * @param speed The duration by which to decrease the speed. Defaults to 500 milliseconds. Must
+     * be a non-negative duration.
+     * @throws IllegalArgumentException if the provided speed duration is negative.
      */
     fun decreaseSpeed(speed: Duration = Duration.ofMillis(500))
 }
--- Jetcaster/core/domain/src/main/java/com/example/jetcaster/core/player/MockEpisodePlayer.kt
+++ Jetcaster/core/domain/src/main/java/com/example/jetcaster/core/player/MockEpisodePlayer.kt
@@ -32,18 +32,120 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
+/**
+ * A mock implementation of the [EpisodePlayer] interface for testing and development purposes.
+ *
+ * This class provides a simulated episode player that allows you to control playback, queue
+ * management, and time manipulation without relying on a real media player.
+ *
+ * The [MockEpisodePlayer] uses Kotlin Coroutines and Flows to manage its state and simulate time progression.
+ *
+ * @property mainDispatcher The coroutine dispatcher used for managing coroutines within the player.
+ */
 class MockEpisodePlayer(
     private val mainDispatcher: CoroutineDispatcher
 ) : EpisodePlayer {
 
-    private val _playerState = MutableStateFlow(EpisodePlayerState())
-    private val _currentEpisode = MutableStateFlow<PlayerEpisode?>(null)
-    private val queue = MutableStateFlow<List<PlayerEpisode>>(emptyList())
-    private val isPlaying = MutableStateFlow(false)
-    private val timeElapsed = MutableStateFlow(Duration.ZERO)
-    private val _playerSpeed = MutableStateFlow(DefaultPlaybackSpeed)
-    private val coroutineScope = CoroutineScope(mainDispatcher)
+    /**
+     * Represents the state of the episode player.
+     *
+     * This could include information such as:
+     * - Is the player currently playing?
+     * - What is the current playback position?
+     * - Is the player paused?
+     * - Is the player buffering?
+     * - What is the current episode being played?
+     * - ...etc.
+     */
+    private val _playerState = MutableStateFlow(value = EpisodePlayerState())
 
+    /**
+     * The currently playing episode in the player.
+     *
+     * This is a [MutableStateFlow] that emits the [PlayerEpisode] representing the
+     * episode that is currently being played. If no episode is currently playing,
+     * the value will be `null`.
+     *
+     * This property is intended for internal use by the player to manage its state.
+     * External components should observe this state via the public property [currentEpisode]
+     *
+     * Updates to this property will cause recomposition if observed in a composable context.
+     */
+    private val _currentEpisode = MutableStateFlow<PlayerEpisode?>(value = null)
+
+    /**
+     * The queue of episodes to be played.
+     *
+     * This is a [MutableStateFlow] that holds a list of [PlayerEpisode] objects.
+     * Changes to this list will be emitted as new states to any collectors of this flow.
+     * The initial state is an empty list.
+     *
+     * Use cases:
+     * - Adding episodes to the playback queue.
+     * - Removing episodes from the playback queue.
+     * - Reordering episodes in the playback queue.
+     * - Observing the current state of the playback queue.
+     * - Displaying the current queue to the user.
+     *
+     * Thread Safety:
+     * Access and modifications to the list within the flow are thread-safe as guaranteed by
+     * [MutableStateFlow].
+     */
+    private val queue = MutableStateFlow<List<PlayerEpisode>>(value = emptyList())
+
+    /**
+     * Indicates whether the media is currently playing.
+     *
+     * This is a [MutableStateFlow] that emits `true` when the media playback has started
+     * and is actively playing, and `false` when the playback is paused, stopped, or
+     * hasn't started yet.
+     */
+    private val isPlaying = MutableStateFlow(value = false)
+
+    /**
+     * Represents the elapsed time since playback started.
+     *
+     * This property holds the current elapsed time as a [Duration] and is updated
+     * over time.  It's represented as a [MutableStateFlow] to allow for reactive
+     * updates and observation of the changing elapsed time.
+     */
+    private val timeElapsed = MutableStateFlow(value = Duration.ZERO)
+
+    /**
+     * The current playback speed of the player.
+     *
+     * This is a [MutableStateFlow] that holds a [Duration] representing the player's speed.
+     * A playback speed of:
+     *  - `1 second` (1.0x) indicates normal playback speed.
+     *  - `2 seconds` (2.0x) indicates playback at double speed.
+     *  - `0.5 seconds` (0.5x) indicates playback at half speed.
+     *
+     * Changes to this value will be reflected in the playback rate of the media.
+     *
+     * The initial value is [DefaultPlaybackSpeed].
+     *
+     * @see DefaultPlaybackSpeed
+     */
+    private val _playerSpeed: MutableStateFlow<Duration> =
+        MutableStateFlow(value = DefaultPlaybackSpeed)
+
+    /**
+     * The [CoroutineScope] used for managing coroutines within the player.
+     */
+    private val coroutineScope = CoroutineScope(context = mainDispatcher)
+
+    /**
+     * The [Job] representing the currently running timer.
+     *
+     * When the timer is started, a new [Job] is created and assigned to this property.
+     * If a timer is already running, the existing [Job] will be cancelled before
+     * creating a new one.
+     *
+     * When the timer is stopped or completed, this property should be set to `null`
+     * to indicate that no timer is currently active.
+     *
+     * This property should be accessed and modified from the coroutine context.
+     */
     private var timerJob: Job? = null
 
     init {
@@ -72,21 +174,98 @@ class MockEpisodePlayer(
         }
     }
 
+    /**
+     * The current speed at which the player is playing.
+     *
+     * This property represents the playback speed as a [Duration].
+     * A value of [Duration.seconds(1)] means normal speed.
+     * Values greater than [Duration.seconds(1)] indicate faster speeds.
+     * Values less than [Duration.seconds(1)] and greater than [Duration.ZERO] indicate slower speeds.
+     * Values of [Duration.ZERO] or less are invalid and will likely result in undefined behavior.
+     *
+     * Note that this is a derived property based on the underlying [_playerSpeed] value.
+     */
     override var playerSpeed: Duration = _playerSpeed.value
 
+    /**
+     * Represents the current state of the episode player.
+     *
+     * This property provides a read-only [StateFlow] reflecting the player's
+     * current state, including loading, playing, paused, error, and completed states.
+     *
+     * You can collect updates from this [StateFlow] to react to changes in the player's
+     * state, such as updating the UI or triggering other actions.
+     *
+     * Note: This is a read-only view of the internal [_playerState]. To modify the state,
+     * use the appropriate methods within the player implementation.
+     */
     override val playerState: StateFlow<EpisodePlayerState> = _playerState.asStateFlow()
 
+    /**
+     * The currently playing episode.
+     *
+     * This property represents the episode that is currently being played by the player.
+     * It can be `null` if no episode is currently loaded or playing.
+     *
+     * Changes to this property will trigger updates to the player's state and UI.
+     */
     override var currentEpisode: PlayerEpisode? by _currentEpisode
+
+    /**
+     * Adds a [PlayerEpisode] to the end of the playback queue.
+     *
+     * This function appends the given [episode] to the current queue of episodes.
+     * The queue is managed internally and updated atomically.
+     *
+     * @param episode The [PlayerEpisode] to be added to the queue.
+     */
     override fun addToQueue(episode: PlayerEpisode) {
         queue.update {
             it + episode
         }
     }
 
+    /**
+     * Removes all items from the queue, effectively emptying it.
+     *
+     * This function sets the underlying queue's value to an empty list,
+     * discarding all currently enqueued items.
+     */
     override fun removeAllFromQueue() {
         queue.value = emptyList()
     }
 
+    /**
+     * Starts or resumes playback of the currently selected episode.
+     *
+     * This function manages the playback state and updates the elapsed time.
+     * It performs the following actions:
+     *
+     * 1. **Checks if already playing:** If [isPlaying] is true, it does nothing and returns early,
+     * preventing multiple playback instances.
+     * 2. **Retrieves the current episode:** It attempts to get the current episode from
+     * [_currentEpisode]. If no episode is found (null), it returns early.
+     * 3. **Sets playing state:** It sets [isPlaying] to true, indicating that playback has started.
+     * 4. **Launches a coroutine:** It starts a coroutine using [coroutineScope] to handle the timer
+     * and playback logic.
+     * 5. **Increments the timer:** Inside the coroutine, it enters a loop that continues as long as:
+     *     - The coroutine is active (`isActive`).
+     *     - The `timeElapsed` is less than the episode's duration.
+     *     - Within the loop a delay is set based on the `playerSpeed` using `playerSpeed.toMillis()`.
+     *     - After the delay the `timeElapsed` is incremented by the `playerSpeed` amount.
+     * 6. **Handles playback completion:** When the loop finishes (either the coroutine is canceled
+     * or the elapsed time equals/exceeds the episode duration):
+     *     - `isPlaying` is set to false, indicating playback has stopped.
+     *     - `timeElapsed` is reset to `Duration.ZERO`.
+     *     - It checks if there is a next episode using `hasNext()`.
+     *     - If there is a next episode, `next()` is called to automatically start playing it.
+     *
+     * **Note:** This function relies on several other properties and functions, including:
+     *  - `isPlaying`: A `MutableStateFlow<Boolean>` representing the current playback state.
+     *  - `_currentEpisode`: A `StateFlow<Episode?>` holding the currently selected episode.
+     *  - `timeElapsed`: A `MutableStateFlow<Duration>` representing the elapsed time in the
+     *  current episode.
+     */
     override fun play() {
         // Do nothing if already playing
         if (isPlaying.value) {
