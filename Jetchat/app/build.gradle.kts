cad0a1780565b1366ac768b8bf5bd38570badff5
Mark Gray <mark.t.gray@gmail.com>
2025-07-02 04:21:27 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in Jetchat

--- Jetchat/app/build.gradle.kts
+++ Jetchat/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 /*
  * Copyright 2020 The Android Open Source Project
  *
@@ -66,8 +68,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
 
     buildFeatures {
